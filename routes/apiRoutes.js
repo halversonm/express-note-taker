c5942b7762d9e59edb617b7e362e772cbe7b4c4a
mh <maxhalverson@live.com>
2023-07-24 09:31:53 -0500

    set up route for getting db.json data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,16 @@
 const router = require("express").Router()
 const { v4: uuidv4 } = require("uuid")
+const {readFromFile, writeToFile, readAndAppend, readAndDelete } = require("../utils/helper")
 
 
 router.get("/notes", (req, res) => {
-    
+    readFromFile("./db/db.json").then((data) => {
+        res.json(JSON.parse(data))
+    })
 })
 
 router.post("/notes", (req, res) => {
-    
+       
 })
 
 router.delete("/notes/:id", (req, res) => {
