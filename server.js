03ca3714edea2e519da2f2cdce05fb743c2b0b01
mh <maxhalverson@live.com>
2023-07-24 09:49:44 -0500

    set up routes

--- server.js
+++ server.js
@@ -1,29 +1,18 @@
 const express = require('express');
-const path = require('path');
-
 // Setting up router for modularizing 
-const api = require('./routes')
-
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Middleware 
-// I need middleware for parsing JSON and urlencoded form data. do i always need both? How do i know what i need?
-// app.use('/api', api); (how do i know if the route is through /api?)
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
